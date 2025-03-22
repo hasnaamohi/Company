420de00af79f8447451d2d16bdc92e1256892a24
hasnaaMohi <hasnaa.mohamed1221@gmail.com>
2025-03-22 03:22:43 +0200

    Add employee.PLL before add generic

---
+++ Company.Mahmoud.PLL/Repositry/EmployeeRepositry.cs
@@ -0,0 +1,49 @@
+﻿using Company.Mahmoud.DAL.Data.Context;
+using Company.Mahmoud.DAL.Models;
+using Company.PLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.PLL.Repositry
+{
+    internal class EmployeeRepositry : IEmployeeRepositry
+    {
+        private readonly CompanyDbContextcs _context;
+        public EmployeeRepositry(CompanyDbContextcs context)
+        {
+            context = _context;
+        }
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+        public Employee? GetById(int id)
+        {
+            return _context.Employees.Find(id);
+        }
+
+        public int add(Employee model)
+        {
+           _context.Employees.Add(model);
+            return _context.SaveChanges();
+            
+        }
+
+    
+        public int update(Employee model)
+        {
+            _context.Employees.Update(model);
+            return _context.SaveChanges();
+        }
+
+        public int delete(Employee model)
+        {
+            _context.Employees.Remove(model);
+            return _context.SaveChanges();
+        }
+
+    }
+}
